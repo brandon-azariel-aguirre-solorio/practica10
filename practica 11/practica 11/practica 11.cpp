be3c24b08a3f56c3f691c1ab169eb078e057aead
Saulo <sau890p@gmail.com>
2019-11-21 11:08:20 -0600

    a

---
+++ practica 11/practica 11/practica 11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int cont = 1;
+	cout << "los primeros numeros naturales ";
+	while (cont <= 100)
+	{
+		cout << cont<<endl;
+		cont = cont + 1;
+
+	}
+	cout << "Aqui ya salio el siclo ";
+	return 0;
+}
